0ca05b18d066740040b31fe149f69f049011ea84
Raffael Raiel Trindade <raffaelraiel@usp.br>
2026-04-04 17:14:37 -0300

    UserPowers and UserStats classes on db. One to One relationship.

--- models.py
+++ models.py
@@ -8,6 +8,23 @@ class User(SQLModel, table=True):
     password: str
     points: int = Field(default=0)
 
+class UserPowers(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id", unique=True)
+    good_start: int = Field(default=0)
+    russian_roulette: int = Field(default=0)
+    mine_freeze: int = Field(default=0)
+    hint: int = Field(default=0)
+
+class UserStats(SQLModel, table=True):
+    id:             int = Field(default=None, primary_key=True)
+    user_id:        int = Field(foreign_key="user.id", unique=True)
+    games_won:      int = Field(default=0)
+    games_lost:     int = Field(default=0)
+    current_streak: int = Field(default=0)
+    best_streak:    int = Field(default=0)
+
+
 def create_tables():
     SQLModel.metadata.create_all(engine)
 
