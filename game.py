bf3b0a4b37fb27a8c32a82c1ac0f7fcb5f84de31
Raffael Raiel Trindade <raffaelraiel@usp.br>
2026-04-07 16:33:29 -0300

    Removed good start power

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ difficulties = {"easy": {"rows": 9, "cols": 9, "mines": 10},
                 "medium": {"rows": 16, "cols": 16, "mines": 40},
                 "hard": {"rows": 16, "cols": 30, "mines": 99}}
 
-power_costs = {"good_start": 30, "russian_roulette": 20,
+power_costs = {"russian_roulette": 20,
                 "mine_freeze": 40, "hint": 25}
 
 points_base = {"easy": 50, "medium": 150, "hard": 400}
