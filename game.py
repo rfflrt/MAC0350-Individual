196cf4fd619783c091cf280b71846f0f736f893f
Raffael Raiel Trindade <raffaelraiel@usp.br>
2026-04-07 15:15:56 -0300

    Created game.py and added difficulties. Home cna run. Not functional.

---
+++ game.py
@@ -0,0 +1,3 @@
+difficulties = {"easy":   {"rows": 9,  "cols": 9,  "mines": 10},
+                "medium": {"rows": 16, "cols": 16, "mines": 40},
+                "hard":   {"rows": 16, "cols": 30, "mines": 99}}
