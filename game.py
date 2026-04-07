efb1f88d6deb3d6e1a53596c4cf8881243811eeb
Raffael Raiel Trindade <raffaelraiel@usp.br>
2026-04-07 15:41:05 -0300

    Game.py eith basic points and initial mine placement. Has 3x3 safe place to start.Better than good start, may remove.

--- game.py
+++ game.py
@@ -1,8 +1,25 @@
+import random
+
 difficulties = {"easy": {"rows": 9, "cols": 9, "mines": 10},
                 "medium": {"rows": 16, "cols": 16, "mines": 40},
                 "hard": {"rows": 16, "cols": 30, "mines": 99}}
 
-power_costs = {"good_start": 30,
-               "russian_roulette": 20,
-                "mine_freeze": 40,
-                "hint": 25}
+power_costs = {"good_start": 30, "russian_roulette": 20,
+                "mine_freeze": 40, "hint": 25}
+
+points_base = {"easy": 50, "medium": 150, "hard": 400}
+
+def place_mines(rows, cols, count, safeR, safeC):
+    safe = {(safeR + r, safeC + c)
+        for r in range(-1, 1) for c in range(-1, 1)
+        if 0 <= safeR+r and safeR+r < rows and
+        0 <= safeC+c and safeC+c < cols}
+    
+    candidates = [(r,c) for r in range(rows) for c in range(cols)
+                  if (r,c) not in safe]
+    mines = random.sample(candidates, count)
+
+    movers_qty = len(mines) * 0.1
+    movers = set(random.sample(range(len(mines)), movers_qty))
+
+    return mines, movers
